8aae564ad2e6ed03af58167028783a2a1384aa64
Arturo Puente <theadpuente@gmail.com>
2023-03-10 19:50:12 +0100

    commit

--- src/components/Notas/TableGrades.jsx
+++ src/components/Notas/TableGrades.jsx
@@ -5,16 +5,16 @@ const TableGrades = ({ id, estudiante, nota }) => {
     return (
 
 
-      
-            <tr>
-                <td>
-                    {estudiante}
-                </td>
-                <td><p className="badge"> {nota} </p>  </td>
-                <td> <Link to={`/student-edit/${id}`}> <button className="button"> Editar el puto niño</button> </Link></td>
-            </tr>
 
-   
+        <tr>
+            <td>
+                {estudiante}
+            </td>
+            <td><p className="badge"> {nota} </p>  </td>
+            <td> <Link to={`/student-edit/${id}`}> <button className="button"> Editar notas</button> </Link></td>
+        </tr>
+
+
 
     )
 }
